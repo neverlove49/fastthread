8c263b9d859f598a93905dbda91afb544b5567a4
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-03-20 08:39:30 +0100

    Make it a no-op on JRuby.

--- ext/fastthread/extconf.rb
+++ ext/fastthread/extconf.rb
@@ -1,16 +1,16 @@
-require 'mkmf'
-
 version_components = RUBY_VERSION.split('.').map { |c| c.to_i }
 
 need_fastthread = ( !defined? RUBY_ENGINE )
 need_fastthread &= ( RUBY_PLATFORM != 'java' )
-need_fastthread &= ( version_components[0..1] == [1, 8] && ( version_components[2] < 6 || version_components[2] == 6 && RUBY_PATCHLEVEL < 111 ) )
+need_fastthread &= ( version_components[0..1] == [1, 8] && ( version_components[2] < 6 || version_components[2] == 6 && RUBY_PATCHLEVEL.to_i < 111 ) )
 
 if need_fastthread
+  require 'mkmf'
   create_makefile('fastthread')
 else
+  require 'rbconfig'
   File.open('Makefile', 'w') do |stream|
-    CONFIG.each do |key, value|
+    Config::CONFIG.each do |key, value|
       stream.puts "#{key} = #{value}"
     end
     stream.puts
